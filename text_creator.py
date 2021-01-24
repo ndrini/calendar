39f88ae498e5eaab6b546cccc5e6cec14fa014de
ndrini <ndrini@gmail.com>
2021-01-24 18:53:24 +0100

    fix two bugs: week counter generally (with only exception on Monday) delayed of one week; consitered cases of not Monday starting month

--- text_creator.py
+++ text_creator.py
@@ -3,12 +3,10 @@ from yaml import safe_load
 class text_creator():
     def __init__(self, 
                  month, 
-                 # first_month_day,
                  week_day,
                  week_number,
                  ):
         self.month = month
-        # self.first_month_day = first_month_day
         self.week_day = week_day
         self.week_number = week_number
 
@@ -19,12 +17,16 @@ class text_creator():
         week_day_number = data['week_days'].index(self.week_day)
         print("# Week by week on {}".
             format(this_month) )
-        for w in range(5):   # for 4 or 5 weeks in a month
-            print("\n__Settimana {}: {} 2021__".format(
-            this_week,
-            this_month)
-            )
+        # check if the week already started 
+        if week_day_number != 0:
+            this_week += 1
+        for _ in range(5):   # for 4 or 5 weeks in a month
             for _ in range(7):
+                if week_day_number % 7  == 0:
+                    print("\n__Settimana {}: {} 2021__".format(
+                        this_week,
+                        this_month)
+                        )    
                 print("  * * {} {} {} *".format(
                 data["week_days"][week_day_number % 7 ],
                 month_day, 
@@ -35,16 +37,10 @@ class text_creator():
                 week_day_number += 1 
             # update the values after a week 
             this_week += 1
-        
-        print ("Array day value: ", data['week_days'].index(self.week_day))
-
-
+    
 with open(r'constants.yaml') as data_file:
     data = safe_load(data_file)
 
-print (data)
-# c = text_creator(2, 1, "mercoledì", 6)
 #              month, week_day, week_number,
-c = text_creator(3, "lunedì", 10)
+c = text_creator(4, "giovedì", 14)
 c.export_as_text()
-
